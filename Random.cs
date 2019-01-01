a285b15df393f3bbcbb9fee5d62e872acc860799
dotnet1612 <46135939+dotnet1612@users.noreply.github.com>
2019-01-01 20:59:33 +0200

    Add files via upload

---
+++ Random.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomEngine = new Random(); // memory cell in type of Random Generator
+
+            for (int i = 1; i <= 100; i++)
+            {
+                //Random randomEngine = new Random(); // not here!!!
+                int a = randomEngine.Next(1, 101);
+                Console.WriteLine(a);
+            }
+            
+        }
+    }
+}
